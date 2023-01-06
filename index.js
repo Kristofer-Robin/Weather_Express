45a922273aeccd87c881173b0ce7585cf7055b5c
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2023-01-06 13:21:57 +0200

    Created API error handling

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -17,11 +20,12 @@ const getWeatherDataPromise = (url) => {
             .then(data => {
                 let description = data.weather[0].description
                 let city = data.name
-                let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+                let temp = Math.round(parseFloat(data.main.temp) -273.15)
                 let result = {
                     description: description,
                     city: city,
-                    temp: temp
+                    temp: temp,
+                    error: null
                 }
                 resolve(result)
             })
@@ -31,23 +35,22 @@ const getWeatherDataPromise = (url) => {
     })
 }
 
-app.get('/', function (req, res) {
-    let city = 'Tartu'
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-    getWeatherDataPromise(url)
-        .then(data => {
-        res.render('index', data)
-    })
-})
+app.all('/', function (req, res) {
+    let city
+    if (req.method === 'POST' && req.body.cityname) {
+        city = req.body.cityname;
+    } else {
+        city = 'Tartu';
+    }
 
-app.post('/', function (req, res) {
-    let city = req.body.cityname
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
     getWeatherDataPromise(url)
         .then(data => {
             res.render('index', data)
         })
+        .catch(error => {
+            res.render('index', {error: 'Problem-with-getting data, try again'})
+        })
 })
-
 
 app.listen(3005)
